c31117320e153921f60ed6a1a0fa9de7165562f4
SabariDeveloper02 <sabariscommunicationpractice@gmail.com>
2026-07-21 14:26:39 +0530

    feat: add DashboardController with navigation and user welcome

---
+++ src/main/java/com/fitsync/controller/DashboardController.java
@@ -0,0 +1,15 @@
+package com.fitsync.controller;
+
+import com.fitsync.FitSyncApp;
+import com.fitsync.model.User;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController {
+    
+}
