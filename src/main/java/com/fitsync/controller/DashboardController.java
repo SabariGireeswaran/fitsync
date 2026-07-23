a497109b98b0bf20a7aed06f18942ad658b87fa6
SabariDeveloper02 <sabariscommunicationpractice@gmail.com>
2026-07-23 09:49:51 +0530

    feat: add DashboardController with navigation and user welcome

--- src/main/java/com/fitsync/controller/DashboardController.java
+++ src/main/java/com/fitsync/controller/DashboardController.java
@@ -1,15 +1,56 @@
 package com.fitsync.controller;
 
-import com.fitsync.FitSyncApp;
-import com.fitsync.model.User;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class DashboardController {
+import com.fitsync.FitSyncApp;
+import com.fitsync.model.User;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class DashboardController implements Initializable{
+    @FXML private Label welcomeLabel;
+    @FXML private Label bmiLabel;
+    @FXML private Label bmiCategoryLabel;
+    @FXML private Label workoutCountLabel;
+    @FXML private Label weightLabel;
+
+    private static User currentUser;
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    @Override 
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        if(currentUser != null){
+            welcomeLabel.setText("Welcome back, " + currentUser.getName() + "!");
+            weightLabel.setText(currentUser.getWeightKg() + "kg");
+        }
+        bmiLabel.setText("--");
+        bmiCategoryLabel.setText("Not calculated yet");
+        workoutCountLabel.setText("0");
+    }
+
+    @FXML 
+    private void showDashboard() {}
     
+    @FXML
+    private void showBmi() {
+        System.out.println("BMI clicked");
+    }
+
+    @FXML
+    private void showWorkout() {
+        System.out.println("Workout clicked");
+    }
+
+    @FXML 
+    private void handleLogout() throws IOException {
+        currentUser = null;
+        FitSyncApp.showLoginScreen();
+    }
 }
